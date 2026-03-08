5af2ab6f992ff97dc2e334c30fa53d0800645609
MuhammedRafzan <166210649+MuhammedRafzan@users.noreply.github.com>
2026-03-08 18:00:38 +0530

    Fix mongoose connection code formatting

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 /* ══════════════════════════════════════
    DB CONNECTION
 ══════════════════════════════════════ */
-const mongoose = require("mongoose");
+
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("✅ MongoDB connected"))
